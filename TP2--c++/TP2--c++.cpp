7ae49fe69a51542893b1c1c417b92415808bbf3c
baptiste1908 <baptiste.gilleron@student.junia.com>
2023-09-20 10:01:08 +0200

    Exo 1,2,3 fini

--- TP2--c++/TP2--c++.cpp
+++ TP2--c++/TP2--c++.cpp
@@ -1,22 +1,100 @@
-// TP2--c++.cpp : Ce fichier contient la fonction 'main'. L'exÃ©cution du programme commence et se termine Ã  cet endroit.
-//
-
-
-
 #include <iostream>
+#include <iomanip>
+#include <locale>
+#include <fstream>
+#include<string>
+#include<sstream>
+
+#define multiplier(x,y) ((x) * (y))
+
+using namespace std;
+using std::cout;
+using std::cin;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "fr-FR");
+
+	// EXERCICE 1
+
+	int a;
+	int b;
+	cout << "Entrez une valeur :" << endl;
+	cin >> a;
+	cout << "Entrez une autre valeur :" << endl;
+	cin >> b;
+	cout << multiplier(a + 1, b + 1) << endl;
+	
+	// LEs 2 lignes suivantes permettent d'effacer l'écran de la console
+	system("PAUSE");
+	system("CLS");
+
+	// EXERCICE 2
+
+	int c;
+	cout << "Entrer un nombre entre 1 et 9 :" << endl; // comme un printf
+	cin >> c; // comme un scanf
+	
+	for (int i = 1; i <= 10; i++) {
+		cout << multiplier(c, i) << " | ";
+	}
+	cout << endl;
+	system("PAUSE");
+	system("CLS");
+
+	// 2e version de la table de multiplication 
+	cout << "Avec la librairie iomanip : " << endl;
+ 	for (int i = 1; i <= 10; i++) {
+		cout << setw(50) << multiplier(c, i) << endl; // la valeur dans setw(..) permet de placer ou l'on veut la colonne de nombre sur la page
+	}
+	system("PAUSE");
+	system("CLS");
+
+	//EXERCICE 3 :  Ecrire un programme C++ qui analyse un texte à partir d’un fichier en affichant le nombre de lignes, de mots et de lettres (l’utilisation de la classe «stringstream» est préconisée voire impératif).
+
+	ifstream file;
+	file.open("text.txt");
+	string line;
+	string word;
+
+	int nbline = 0;
+	int nbword = 0;
+	int nblettres = 0;
+
+	int tab[255] = {};
+
+	if (file.is_open()) {  // On ouvre le fichier texte
+		while (!file.eof()) { // tant qu'il n'est pas fermé
+			getline(file, line); // on récupere le nombre de lignes
+			nbline++;			// On incrémente alors notre nombre de lignes
+			stringstream ss(line);   // stringstream permet à une chaîne de caractères d'être traitée comme un flux 
+			while (ss >> word) {    // tant que la ligne à des mots 
+				for (auto &c : word) { // on regarde les mots
+					c = tolower(c);				// met tt en minuscule
+					if (isalpha(c)) ++tab[c];  // isalpha permet de voir si le caractere est alphabéthique et post-incrémente le tableau
+				}
+				nbword++; // on incremente alors le nombre de mot
+				nblettres += word.size(); // on incremente ensuite le nombre de lettre en fonction de la taille des mots
+				}
+			}
+		}
+	file.close();  // Enfin, on ferme le fichier 
+	// Et on affiche les lettres, lignes, mots.
+	cout << "Le nombre de line est de " << nbline << endl;
+	cout << "Le nombre de mots est de " << nbword << endl;
+	cout << "Le nombre de lettres est de " << nblettres << endl;
+
+	system("PAUSE");
+	system("CLS");
+
+	for (auto i = 0; i < 255; i++)
+	{
+		if (tab[i]) cout << char(i) << " : " << tab[i] << endl;
+	}
+	system("PAUSE");
+	system("CLS");
 }
 
-// ExÃ©cuter le programmeÂ : Ctrl+F5 ou menu DÃ©boguerÂ > ExÃ©cuter sans dÃ©bogage
-// DÃ©boguer le programmeÂ : F5 ou menu DÃ©boguerÂ > DÃ©marrer le dÃ©bogage
 
-// Astuces pour bien dÃ©marrerÂ : 
-//   1. Utilisez la fenÃªtre Explorateur de solutions pour ajouter des fichiers et les gÃ©rer.
-//   2. Utilisez la fenÃªtre Team Explorer pour vous connecter au contrÃ´le de code source.
-//   3. Utilisez la fenÃªtre Sortie pour voir la sortie de la gÃ©nÃ©ration et d'autres messages.
-//   4. Utilisez la fenÃªtre Liste d'erreurs pour voir les erreurs.
-//   5. AccÃ©dez Ã  ProjetÂ > Ajouter un nouvel Ã©lÃ©ment pour crÃ©er des fichiers de code, ou Ã  ProjetÂ > Ajouter un Ã©lÃ©ment existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accÃ©dez Ã  FichierÂ > OuvrirÂ > Projet et sÃ©lectionnez le fichier .sln.
+	
---
+++ TP2--c++/TP2.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <iomanip>
+#include <locale>
+#include <fstream>
+#include<string>
+#include<sstream>
+#define multiplier(x,y) ((x) * (y))
